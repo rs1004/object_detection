e1d1bb01f012ca39e0109d1ce4901c33d7ba4ae7
rs1004 <r82.y25@gmail.com>
2021-04-16 21:07:37 +0900

    [add] loss

--- src/models/ssd.py
+++ src/models/ssd.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torchvision.models import vgg16_bn
 from collections import Counter
+from torchvision.ops import box_iou, box_convert
 try:
     from .layers import ConvBlock, L2Norm
 except ImportError:
@@ -147,9 +148,100 @@ class SSD(nn.Module):
         dboxes = torch.tensor(dboxes)
         return dboxes
 
-    def loss(self, out_ls, out_cs, bboxes, labels):
-        # dboxes = self.dboxes.to(bboxes.device)
-        pass
+    def loss(self, out_ls: torch.Tensor, out_cs: torch.Tensor, batch_bboxes: list, batch_labels: list,
+             iou_thresh: float = 0.5, alpha: float = 1.0) -> tuple:
+        """ 損失関数
+
+        Args:
+            out_ls (torch.Tensor): 予測オフセット (B, D, 4) (fmt: [Δcx, Δcy, Δw, Δh])
+                                   D: DBoxの数. D = 8732 の想定.
+            out_cs (torch.Tensor): 予測信頼度 (B, D, 21)
+            batch_bboxes (list): 正解BBOX座標 [(G1, 4), (G2, 4), ...] (fmt: [cx, cy, w, h])
+            batch_labels (list): 正解ラベル [(G1,), (G2,)]
+            iou_thresh (float): Potitive / Negative を判定する際の iou の閾値
+
+        Returns:
+            tuple: (loss, localization loss, classification loss)
+        """
+
+        device = out_ls.device
+        dboxes = self.dboxes.to(device)
+        N = out_ls.size(0)
+        loss_all = loss_l = loss_c = 0
+        for out_l, out_c, bboxes, labels in zip(out_ls, out_cs, batch_bboxes, batch_labels):
+            # to GPU
+            bboxes = bboxes.to(device)
+            labels = labels.to(device)
+
+            # [Step 1]
+            #   各 Default Box を bbox に対応させ、Positive, Negative の判定を行う
+            #   * max_iou >= 0.5 の場合、Positive Box とみなし、最大 iou の bbox を対応させる
+            #   * max_iou <  0.5 の場合、Negative Box とみなす
+            bboxes_xyxy = box_convert(bboxes, in_fmt='cxcywh', out_fmt='xyxy')
+            dboxes_xyxy = box_convert(dboxes, in_fmt='cxcywh', out_fmt='xyxy')
+            max_ious, indices = box_iou(dboxes_xyxy, bboxes_xyxy).max(dim=1)
+            pos_ids, neg_ids = (max_ious >= iou_thresh).nonzero().reshape(-1), (max_ious < iou_thresh).nonzero().reshape(-1)
+
+            # [Step 2]
+            #   Positive Box に対して、 Localization Loss を計算する
+            bboxes_pos = bboxes[indices[pos_ids]]
+            dboxes_pos = dboxes[pos_ids]
+            dbboxes_pos = self._calc_delta(bboxes=bboxes_pos, dboxes=dboxes_pos)
+            loss_l += self._smooth_l1(out_l[pos_ids] - dbboxes_pos).sum()
+
+            # [Step 3]
+            #   Positive / Negative Box に対して、Confidence Loss を計算する
+            #   * Negative Box の label は 0 とする (Positive Box の label を 1 ずらす)
+            #   * Negative Box は Loss の上位 len(pos_ids) * 3 個のみを計算に使用する (Hard Negative Mining)
+            labels = labels[indices] + 1
+            labels[neg_ids] = 0
+            sce = self._softmax_cross_entropy(out_c, labels)
+            loss_c += sce[pos_ids].sum() + sce[neg_ids].topk(k=len(pos_ids) * 3).values.sum()
+
+        # [Step 4]
+        #   損失の和を計算する
+        loss_l /= N
+        loss_c /= N
+        loss_all = loss_c + alpha * loss_l
+
+        return loss_all, loss_l, loss_c
+
+    def _calc_delta(self, bboxes: torch.Tensor, dboxes: torch.Tensor, variance: list = [0.1, 0.2]) -> torch.Tensor:
+        """ Δg を算出する
+
+        Args:
+            bboxes (torch.Tensor, [X, 4]): BBox
+            dboxes (torch.Tensor, [X, 4]): Default Box
+            variance (list, optional): 係数. Defaults to [0.1, 0.2].
+
+        Returns:
+            torch.Tensor: [X, 4]
+        """
+        db_cx = (1 / variance[0]) * (bboxes[:, 0] - dboxes[:, 0]) / dboxes[:, 2]
+        db_cy = (1 / variance[0]) * (bboxes[:, 1] - dboxes[:, 1]) / dboxes[:, 3]
+        db_w = (1 / variance[1]) * (bboxes[:, 2] / dboxes[:, 2]).log()
+        db_h = (1 / variance[1]) * (bboxes[:, 3] / dboxes[:, 3]).log()
+
+        dbboxes = torch.stack([db_cx, db_cy, db_w, db_h], dim=1)
+        return dbboxes
+
+    def _smooth_l1(self, x: torch.Tensor) -> torch.Tensor:
+        """ smooth l1 を計算する
+        Args:
+            x (torch.Tensor): any tensor
+        Returns:
+            torch.Tensor : calculated result
+        """
+        return torch.where(x.abs() < 1, 0.5 * x * x, x.abs() - 0.5)
+
+    def _softmax_cross_entropy(self, pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """ softmax cross entropy を計算する
+        Args:
+            x (torch.Tensor): any tensor
+        Returns:
+            torch.Tensor : calculated result
+        """
+        return -nn.functional.log_softmax(pred, dim=-1)[range(len(target)), target]
 
 
 if __name__ == '__main__':
@@ -161,3 +253,10 @@ if __name__ == '__main__':
     print(out_ls.shape, out_cs.shape)
     for coord in model.dboxes:
         print(coord)
+
+    out_ls = torch.rand(4, 8732, 4)
+    out_cs = torch.rand(4, 8732, 21)
+    batch_bboxes = [torch.rand(5, 4) for _ in range(4)]
+    batch_labels = [torch.randint(0, 20, (5,)) for _ in range(4)]
+
+    print(model.loss(out_ls, out_cs, batch_bboxes, batch_labels))
